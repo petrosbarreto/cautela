9b21a48babb3698227ce0d0e532a332005ed66cf
Yuri Fialho <yurirfialho@gmail.com>
2015-10-17 10:31:00 -0300

    Removido arquivos obsoletos e adicionado o model item

---
+++ models/Item.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once dirname(__FILE__) . '/../libs/phpactiverecord/ActiveRecord.php';
+
+class Item extends ActiveRecord\Model {
+	#config
+	static $table_name = 'item';
+	#relacionamentos
+	static $belongs_to = array(
+		array('item_tipo', 'class_name' => 'ItemTipo', 'foreign_key' => 'item_tipo_id')
+	);
+}
+
+?>
